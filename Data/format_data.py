84c2a06a7468e77e7286f0d9facfaec20489b792
Josh Mayanja <Joshua.Mayanja@nuix.com>
2024-03-08 22:58:19 -0500

    Initial Commit

---
+++ Data/format_data.py
@@ -0,0 +1,86 @@
+"""
+Script to format all data into quarters and join the two datasets into a single csv file.
+"""
+
+import os
+import pandas as pd
+
+month_dict ={
+    "enero": "Q1",
+    "febrero": "Q1",
+    "marzo": "Q1",
+    "abril": "Q2",
+    "mayo": "Q2",
+    "junio": "Q2",
+    "julio": "Q3",
+    "agosto": "Q3",
+    "septiembre": "Q3",
+    "octubre": "Q4",
+    "noviembre": "Q4",
+    "diciembre": "Q4"
+}
+
+
+def create_quarter_data_dict(df):
+    """Function to create an object to hold quarter data while it is calculated"""
+    return {col: 0 for col in df.columns}
+
+
+def format_quarter_data(df, data):
+    """Function to format quarter data to be saved in the final dataset"""
+    formatted_data = []
+    for col in df.columns:
+        if col == 'Month':
+            formatted_data.append(data["DATE"])
+            formatted_data.append(data["Quarter"])
+        else:
+            formatted_data.append(data[col])
+
+    return formatted_data
+
+
+# Load data
+energy_consumption = pd.read_csv('Energy Consumption_r.csv')
+gdp = pd.read_csv('GDP-1.csv')
+
+# Map data together
+data = []
+quarter_data = create_quarter_data_dict(energy_consumption)
+counter = 0
+
+# Process energy data
+for i in range(len(energy_consumption)):
+    date = energy_consumption["Month"][i].split()
+    quarter = f"{date[0]}{month_dict[date[1]]}"
+
+    # Calculate quarter data
+    for col in energy_consumption.columns:
+        if col == 'Month':
+            quarter_data["DATE"] = quarter
+            quarter_data["Quarter"] = month_dict[date[1]]
+        else:
+            quarter_data[col] += energy_consumption[col][i]
+    counter += 1
+
+    # Reset quarter
+    if counter == 3:
+        formatted_quarter_data = format_quarter_data(energy_consumption, quarter_data)
+        data.append(formatted_quarter_data)
+        quarter_data = create_quarter_data_dict(energy_consumption)
+        counter = 0
+
+
+# Initialize columns
+columns = ['DATE', 'Quarter']
+for col in energy_consumption.columns:
+    if col != 'Month':
+        columns.append(col)
+
+# Create final data frame
+df = pd.DataFrame(data, columns=columns)
+
+# Concatenate with GDP data
+df = pd.merge(df, gdp, on='DATE', how='inner')
+
+# Save formatted data
+df.to_csv('formatted_data.csv', index=False)
---
+++ EDA/clusters.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+# Load Data
+data = pd.read_csv('../Data/formatted_data.csv')
+test_data = data.drop(['DATE', 'Quarter'], axis=1)
+
+# Fit K-means to optimal number of clusters
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(test_data)
+
+# Reduce dimension of test data to 2 so that it can be plotted
+pca = PCA(n_components=2)
+pca_data = pca.fit_transform(test_data)
+
+# Plot results
+x_data = [i[0] for i in pca_data]
+y_data = [i[1] for i in pca_data]
+point_data = [data['Quarter'][i] for i in range(len(data))]
+colors = ['red', 'blue', 'green', 'black']
+color_map = [colors[i] for i in kmeans.labels_]
+
+plt.figure(figsize=(6, 4))
+for i in range(len(x_data)):
+    plt.scatter(x_data[i], y_data[i], marker=f'$Q{point_data[i]}$', c=color_map[i], s=100)
+
+plt.title('Clustered Quarterly Energy Data')
+plt.savefig('clusters.png')
